4e444ea82560e3bdf38228db76ec7fd42c09f250
zhanglingqi <zhanglingqi@qq.com>
2018-10-16 18:33:16 +0800

    登录页面的完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入公共的css
+import '@/assets/css/style.css'
+//引入首页特效的
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+//引入element ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false
 
---
+++ src/views/login/login.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <vue-particles class="log-bg" color="#ccc"></vue-particles>
+        <div class="login-wrap">
+        <el-form
+        class="login-form"
+        label-position="top"
+        label-width="80px"
+        :model="formData">
+        <h1>用户登录</h1>
+        <el-form-item  label="用户名">
+            <el-input  class="ipt" v-model="formData.username"></el-input>
+        </el-form-item>
+        <el-form-item  label="密码">
+            <el-input class="ipt" type="password" v-model="formData.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-button class="btn" type="primary">登录</el-button>
+        </el-form-item>
+        </el-form>
+  </div>
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+   data() {
+       return {
+            formData: {
+                username: '',
+                password: ''
+            }
+       }
+   },
+  components: {}
+}
+</script>
+
+<style scoped >
+    #particles-js {
+        background: url('../../assets/images/darksky.jpg');
+        background-size: 100% 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+    }
+    h1{
+        color: #00eaff;
+    }
+ 
+   
+    .login-wrap {
+    /* background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center; */
+    position: absolute;
+    right: 16%;
+    top: 31%;
+    }
+    .login-wrap .login-form {
+    /* background-color: #fff; */
+    width: 400px;
+    border-radius: 5px;
+    padding: 30px;
+    }
+    .login-wrap .login-form .btn {
+    width: 100%;
+    background-color: #32c5d2;
+    }
+</style>
